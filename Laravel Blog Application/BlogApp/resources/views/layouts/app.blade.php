6147302e9b42b2a34919e78a9b6b903de05394eb
glenroylogan <30448127+glenroylogan@users.noreply.github.com>
2022-04-24 02:57:41 -0500

    This Is A CRUD, Access Control And User Authentication Application For A Website Blog Using Laravel Framework Version 8.83.9
    This Is A CRUD (Create, Read, Update and Delete), Access Control And User Authentication Application For A Website Blog Using Laravel Framework Version 8.83.9 With Documentation Provided as to how the software works.

---
+++ Laravel Blog Application/BlogApp/resources/views/layouts/app.blade.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- CK Editor (for editing posts) -->
+    <script src="https://cdn.ckeditor.com/ckeditor5/34.0.0/classic/ckeditor.js"></script>
+</head>
+<body>
+    <div id="app">
+        <main class="py-4">
+            @include('inc.navbar')
+            <div class="container">
+                @include('inc.messages')
+                @yield('content')
+            </div>   
+            <script>
+                ClassicEditor
+                        .create( document.querySelector( '#editor' ) )
+                        .then( editor => {
+                                console.log( editor );
+                        } )
+                        .catch( error => {
+                                console.error( error );
+                        } );
+            </script>         
+        </main>
+    </div>
+</body>
+</html>
